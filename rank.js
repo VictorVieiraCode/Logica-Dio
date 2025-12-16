255d0505033c4baa2eb7efb0e59b385b1a414214
VictorVieiraCode <victorhugovieira@hotmail.com>
2025-12-16 19:09:06 -0300

    Second

--- rank.js
+++ rank.js
@@ -3,5 +3,7 @@ let vitorias = 10;
 let derrotas = 1;
 let saldo = vitorias - derrotas;
 
+if (saldo < 10) {nivel = "Ferro";}
+else if (saldo >= 10 && xp <= 20) {nivel = "Bronze";}
 
 console.log(saldo);
