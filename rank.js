b09bb174d0a4a4433e7672f528c8deb79fa190af
VictorVieiraCode <victorhugovieira@hotmail.com>
2025-12-17 19:48:24 -0300

    First

--- rank.js
+++ rank.js
@@ -1,9 +1,10 @@
 
-let vitorias = 20;
+let vitorias = 12;
 let derrotas = 1;
 let saldo = vitorias - derrotas;
 
-if (saldo < 10) {saldo = "Ferro";}
-else if (saldo >= 10 && saldo <= 20) {saldo = "Bronze";}
+if (saldo <= 10) {saldo = "Ferro";}
+else if (saldo >= 11 && saldo <= 20) {saldo = "Bronze";}
+else if (saldo >= 21 && saldo <= 30) {saldo = "Prata"}
 
 console.log(`o nivel é ${saldo}. `);
