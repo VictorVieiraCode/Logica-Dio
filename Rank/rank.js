a862a9c8613473956488df7869684a42e978b370
VictorVieiraCode <victorhugovieira@hotmail.com>
2025-12-17 20:06:30 -0300

    Atualiza rank.js

--- Rank/rank.js
+++ Rank/rank.js
@@ -6,5 +6,7 @@ let saldo = vitorias - derrotas;
 if (saldo <= 10) {saldo = "Ferro";}
 else if (saldo >= 11 && saldo <= 20) {saldo = "Bronze";}
 else if (saldo >= 21 && saldo <= 30) {saldo = "Prata"}
+else if (saldo >= 31 && saldo <= 40) {saldo = "Ouro"}
+else if (saldo >= 41 && saldo <= 50) {saldo = "Ouro"}
 
 console.log(`o nivel é ${saldo}. `);
